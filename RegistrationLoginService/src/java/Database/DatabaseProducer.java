0fbf22fcf9c085a99d4d07b37a8f8897f6fc7d3e
Lorenzo Paolo Cocchinone <36743564+laurus96@users.noreply.github.com>
2019-12-30 11:16:43 +0100

    Added DatabaseProducer

---
+++ RegistrationLoginService/src/java/Database/DatabaseProducer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import javax.enterprise.inject.Produces;
+import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+
+
+/**
+ *
+ * @author lorenzopaolococchinone
+ */
+public class DatabaseProducer {
+    
+    @Produces
+    @PersistenceContext(unitName = "BookPU")
+    private EntityManager em;
+    
+}
