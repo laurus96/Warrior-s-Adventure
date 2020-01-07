9a448371b9c7090dba4fe5b46981bc7f34f41db1
laurus96 <lorenzo.cocchinone@gmail.com>
2020-01-07 13:10:16 +0100

    Switch Server
    Cambiato server glassfish 5 --> payara 5
    
    I deu errori rilevati in fase di deploy sono un bug dovuto alla 5° versione di GlassFish

--- WarriorsAdventureServices/build/generated-sources/ap-source-output/Entity/Personaggio_.java
+++ WarriorsAdventureServices/build/generated-sources/ap-source-output/Entity/Personaggio_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T10:30:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T12:32:08")
 @StaticMetamodel(Personaggio.class)
 public class Personaggio_ { 
 
