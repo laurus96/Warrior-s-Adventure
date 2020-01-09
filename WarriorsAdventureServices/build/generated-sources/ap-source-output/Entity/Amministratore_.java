1b577f0510432ff72fe8ad364adf587f1218b528
Giuseppe Arienzo <55873294+Tempesta58@users.noreply.github.com>
2020-01-09 20:42:20 +0100

    problemi

--- WarriorsAdventureServices/build/generated-sources/ap-source-output/Entity/Amministratore_.java
+++ WarriorsAdventureServices/build/generated-sources/ap-source-output/Entity/Amministratore_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T15:35:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T16:11:18")
 @StaticMetamodel(Amministratore.class)
 public class Amministratore_ { 
 
